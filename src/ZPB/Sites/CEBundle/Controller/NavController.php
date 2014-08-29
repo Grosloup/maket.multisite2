e4869b15b2eb18d64b3b3590e8dc8e2f58357370
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-08-29 12:05:36 +0200

    sauvegarde

---
+++ src/ZPB/Sites/CEBundle/Controller/NavController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nicolas Canfrère
+ * Date: 29/08/14
+ * Time: 11:36
+ */
+  /*
+           ____________________
+  __      /     ______         \
+ {  \ ___/___ /       }         \
+  {  /       / #      }          |
+   {/ ô ô  ;       __}           |
+   /          \__}    /  \       /\
+<=(_    __<==/  |    /\___\     |  \
+   (_ _(    |   |   |  |   |   /    #
+    (_ (_   |   |   |  |   |   |
+      (__<  |mm_|mm_|  |mm_|mm_|
+*/
+
+namespace ZPB\Sites\CEBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class NavController extends Controller
+{
+    public function secondNavbarAction()
+    {
+        return $this->render('ZPBSitesCEBundle:Nav:second-navbar.html.twig', []);
+    }
+} 
---
+++ src/ZPB/Sites/ProBundle/Controller/FooterController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nicolas Canfrère
+ * Date: 29/08/14
+ * Time: 11:40
+ */
+  /*
+           ____________________
+  __      /     ______         \
+ {  \ ___/___ /       }         \
+  {  /       / #      }          |
+   {/ ô ô  ;       __}           |
+   /          \__}    /  \       /\
+<=(_    __<==/  |    /\___\     |  \
+   (_ _(    |   |   |  |   |   /    #
+    (_ (_   |   |   |  |   |   |
+      (__<  |mm_|mm_|  |mm_|mm_|
+*/
+
+namespace ZPB\Sites\ProBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class FooterController extends Controller
+{
+    public function mainAction()
+    {
+        return $this->render('ZPBSitesProBundle:Footer:main.html.twig', []);
+    }
+} 
